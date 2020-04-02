2d9e6c3ae9855b2c0838f26190c81d5bb2bb0154
subhayuroy <subhayu.roy2001@gmail.com>
2020-04-02 13:12:03 +0530

    Updating the project

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1024))
+s.connect((socket.gethostname(), 1025))   #1025 PPort number
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+complete_info = ''
+
+while True:
+    msg = s.recv(1024)  # 1024 Bytes
+    if len(msg)<=0:
+        break
+    complete_info += msg.decode("utf-8")
+print(complete_info)
